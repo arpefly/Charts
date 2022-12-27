d5c4643aebc14d623b1a1852363cb32c3dd8b987
arpefly <ugarvlog@gamil.com>
2022-12-27 23:25:39 +0500

    Добавьте файлы проекта.

---
+++ Charts/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Charts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelScale = new System.Windows.Forms.Label();
+            this.labelXScale = new System.Windows.Forms.Label();
+            this.labelYScale = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.buttonStartCalc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(761, 505);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_Paint);
+            // 
+            // labelScale
+            // 
+            this.labelScale.AutoSize = true;
+            this.labelScale.Location = new System.Drawing.Point(683, 520);
+            this.labelScale.Name = "labelScale";
+            this.labelScale.Size = new System.Drawing.Size(56, 13);
+            this.labelScale.TabIndex = 1;
+            this.labelScale.Text = "Масштаб:";
+            // 
+            // labelXScale
+            // 
+            this.labelXScale.AutoSize = true;
+            this.labelXScale.Location = new System.Drawing.Point(745, 520);
+            this.labelXScale.Name = "labelXScale";
+            this.labelXScale.Size = new System.Drawing.Size(32, 13);
+            this.labelXScale.TabIndex = 2;
+            this.labelXScale.Text = "X 1 : ";
+            // 
+            // labelYScale
+            // 
+            this.labelYScale.AutoSize = true;
+            this.labelYScale.Location = new System.Drawing.Point(745, 533);
+            this.labelYScale.Name = "labelYScale";
+            this.labelYScale.Size = new System.Drawing.Size(32, 13);
+            this.labelYScale.TabIndex = 2;
+            this.labelYScale.Text = "Y 1 : ";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 40;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // buttonStartCalc
+            // 
+            this.buttonStartCalc.Location = new System.Drawing.Point(12, 523);
+            this.buttonStartCalc.Name = "buttonStartCalc";
+            this.buttonStartCalc.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartCalc.TabIndex = 3;
+            this.buttonStartCalc.Text = "Start Calc";
+            this.buttonStartCalc.UseVisualStyleBackColor = true;
+            this.buttonStartCalc.Click += new System.EventHandler(this.ButtonStartCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 562);
+            this.Controls.Add(this.buttonStartCalc);
+            this.Controls.Add(this.labelYScale);
+            this.Controls.Add(this.labelXScale);
+            this.Controls.Add(this.labelScale);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelScale;
+        private System.Windows.Forms.Label labelXScale;
+        private System.Windows.Forms.Label labelYScale;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button buttonStartCalc;
+    }
+}
+
---
+++ Charts/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Threading;
+using System.Reflection;
+
+namespace Charts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        readonly int _scaleX = 1; // Масштаб X
+        readonly int _scaleY = 1; // Масштаб Y
+        int _X0 = 400;   // Центр координат по X
+        int _Y0 = 400;   // Центр координат по Y
+        readonly bool _isGridShown = true;   // Показывать сетку координат
+
+        double _X;   // Координата X
+        double _Y;   // Координата Y
+        double _step;    // Шаг счёта
+
+        private Image _buffer;
+
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            _X0 = pictureBox.Width / 2;
+            _Y0 = pictureBox.Height / 2;
+
+            Pen pen = new Pen(Color.SkyBlue, 1);
+
+            #region Сетка
+            if (_isGridShown)
+            {
+                for (int i = 4; i < pictureBox.Width; i += 4)
+                    e.Graphics.DrawLine(pen, i, 1, i, pictureBox.Height - 2);
+                for (int i = 4; i < pictureBox.Height; i += 4)
+                    e.Graphics.DrawLine(pen, 1, i, pictureBox.Width - 2, i);
+            }
+            #endregion
+            #region Обводка
+            pen.Color = Color.Black;
+            e.Graphics.DrawLine(pen, 0, 0, pictureBox.Width, 0);
+            e.Graphics.DrawLine(pen, pictureBox.Width - 1, 0, pictureBox.Width - 1, pictureBox.Height);
+            e.Graphics.DrawLine(pen, 0, 0, 0, pictureBox.Width);
+            e.Graphics.DrawLine(pen, 0, pictureBox.Height - 1, pictureBox.Width - 1, pictureBox.Height - 1);
+            #endregion
+            #region Оси
+            e.Graphics.DrawLine(pen, 0, pictureBox.Height / 2, pictureBox.Width, pictureBox.Height / 2);
+            e.Graphics.DrawLine(pen, pictureBox.Width / 2, 0, pictureBox.Width / 2, pictureBox.Height);
+            #endregion
+            #region Деления и подписи осей
+            // маленькие деления
+            for (int i = 0; i < pictureBox.Width; i += 4)   // по горизонтали
+                e.Graphics.DrawLine(pen, i, pictureBox.Height / 2 - 2, i, pictureBox.Height / 2 + 2);
+            for (int i = 0; i < pictureBox.Height; i += 4)  // по вертикали
+                e.Graphics.DrawLine(pen, pictureBox.Width / 2 - 2, i, pictureBox.Width / 2 + 2, i);
+
+            // большие деления
+            int count = 1;
+            for (int i = pictureBox.Width / 2; i < pictureBox.Width; i += 40)   // справа от центра
+            {
+                e.Graphics.DrawLine(pen, i, pictureBox.Height / 2 - 4, i, pictureBox.Height / 2 + 4);
+                this.Controls.Add(new Label { Text = count++.ToString(), Location = new Point(i + 45, pictureBox.Height / 2 + 25), AutoSize = true });
+            }
+            count = -1;
+            for (int i = pictureBox.Width / 2; i >= 0; i -= 40) // слева от центр
+            {
+                e.Graphics.DrawLine(pen, i, pictureBox.Height / 2 - 4, i, pictureBox.Height / 2 + 4);
+                this.Controls.Add(new Label { Text = count--.ToString(), Location = new Point(i - 35, pictureBox.Height / 2 + 25), AutoSize = true });
+            }
+            count = -1;
+            for (int i = pictureBox.Height / 2; i < pictureBox.Height; i += 40) // снизу от центра
+            {
+                e.Graphics.DrawLine(pen, pictureBox.Width / 2 - 4, i, pictureBox.Width / 2 + 4, i);
+                this.Controls.Add(new Label { Text = count++.ToString(), Location = new Point(pictureBox.Width / 2 + 25, i + 45), AutoSize = true });
+            }
+            count = 1;
+            for (int i = pictureBox.Height / 2; i >= 0; i -= 40)    // сверху от центра
+            {
+                e.Graphics.DrawLine(pen, pictureBox.Width / 2 - 4, i, pictureBox.Width / 2 + 4, i);
+                this.Controls.Add(new Label { Text = count--.ToString(), Location = new Point(pictureBox.Width / 2 + 25, i - 35), AutoSize = true });
+            }
+
+            this.Controls.OfType<Label>().ToList().ForEach(label => label.BringToFront());  // Поднять все label нв верх
+            #endregion
+            #region Посторение графиков
+            if (!(_buffer is null))
+            {
+                e.Graphics.DrawImageUnscaled(_buffer, 0, 0);
+            }
+            #endregion
+        }
+
+        private void DrawToBuffer(Image image)
+        {
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                _X = -10;
+                _step = 0.001;
+
+                for (int i = 1; i < 100000; i++)
+                {
+                    _Y = Math.Sin(_X) + Math.Cos(_X) + Math.Abs(Math.Sin(_X) * Math.Cos(_X));
+                    g.FillRectangle(new SolidBrush(Color.Red), (int)(_X0 + Math.Round(_X * 40 * _scaleX)), (int)(_Y0 - Math.Round(_Y * 40 * _scaleY)), 1, 1);
+                    _X += _step;
+                }
+            }
+        }
+        private async void ButtonStartCalc_Click(object sender, EventArgs e)
+        {
+            if (_buffer is null)
+            {
+                _buffer = new Bitmap(pictureBox.Width, pictureBox.Height);
+            }
+
+            timer.Enabled = true;
+            await Task.Run(() => DrawToBuffer(_buffer));
+            timer.Enabled = false;
+            pictureBox.Invalidate();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            pictureBox.Invalidate();
+        }
+    }
+}
